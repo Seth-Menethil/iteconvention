8c131d5d9b6c194fec9c8815b5cd3768381c86dc
Seth-Menethil <justintan158@gmail.com>
2025-02-11 10:17:03 +0800

    Update on Main Working

--- iteconvention/connection/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    // Create connection
-    $con = mysqli_connect('localhost', 'cybersum_db', 'cybersummit', 'cybersum_db');
-
-    if (!$con) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
-?>
