ec9f7aefa495849de7735021f9d1daa84632900b
Seth-menethil <justintan158@gmail.com>
2025-02-17 14:39:54 +0800

    Edit: Logout JS Edit

--- function/logout.js
+++ function/logout.js
@@ -1,6 +1,6 @@
 function logout() {
     if (confirm('Are you sure you want to logout?')) {
-        window.location.href = "https://iteconvention.com/function/logout.php"; // Corrected this line
+        window.location.href = "./function/logout.php"; // Corrected this line
     } else {
         console.log("Logout cancelled");
     }
