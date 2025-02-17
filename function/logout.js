bfc6092bb73bc8ef337d56584d7b99ae8e8bd361
Seth-menethil <justintan158@gmail.com>
2025-02-17 14:24:05 +0800

    Edit: Changes in Logout.js

--- function/logout.js
+++ function/logout.js
@@ -1,6 +1,6 @@
 function logout() {
     if (confirm('Are you sure you want to logout?')) {
-        window.location.href = "function/logout.php"; // Corrected this line
+        window.location.href = "logout.php"; // Corrected this line
     } else {
         console.log("Logout cancelled");
     }
