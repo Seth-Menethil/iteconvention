03670b1f855a6681bb641b940991bd67636f017d
Seth-menethil <justintan158@gmail.com>
2025-02-17 12:40:10 +0800

    Added Image of Dr. Mary jane

--- index.php
+++ index.php
@@ -232,7 +232,7 @@
 
         <!-- Speaker Card 2 -->
         <div class="card">
-          <img src="assets/img/speaker (2).jpg" alt="Sarah Doe" class="card-img">
+          <img src="assets/img/Mary_Jane.jpg" alt="Sarah Doe" class="card-img">
           <div class="card-body">
             <h3 class="card-title">Dr. Mary Jane Calagui</h3>
             <p class="card-text">Professor V</p>
